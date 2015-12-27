5de5296b047cdf52d19bfa9ca226b79e2f514d2f
Mohamed Mansour <hello@mohamedmansour.com>
2015-12-27 15:23:54 -0800

    Initial MVVM Native Skeleton for MyElectri

---
+++ MyElectricCar/ViewModel/ViewModelBase.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MyElectricCar.ViewModel
+{
+    /// <summary>
+    /// Base class for all view models. Contains the common implementation of 
+    /// INotifyPropertyChanged and the notification helper method.
+    /// </summary>
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Views subscribe to this event to get notified of property updates.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Notify subscribers of updates to the named property
+        /// </summary>
+        /// <param name="propertyName">The full, case-sensitive, name of a property.</param>
+        protected void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                PropertyChangedEventArgs args = new PropertyChangedEventArgs(propertyName);
+                handler(this, args);
+            }
+        }
+    }
+}
---
+++ MyElectricCar/ViewModel/ViewModelLocator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyElectricCar.ViewModel
+{
+    /// <summary>
+    /// ViewModelLocator ensures that viewmodels can be instantiated with a common reference to the QuestionStore. 
+    /// This allows for easier decoupling of the store implementation and the view models, and allows for 
+    /// less viewmodel specific code in the views.
+    /// </summary>
+    public class ViewModelLocator
+    {
+        private Dictionary<string, ViewModelBase> modelSet = new Dictionary<string, ViewModelBase>();
+
+        /// <summary>
+        /// Set up all of the known view models, and instantiate the question repository.
+        /// </summary>
+        public ViewModelLocator()
+        {
+            //QuestionStore store = new QuestionStore();
+            //InitializeStore(store);
+            //modelSet.Add("QuestionListViewModel", new QuestionListViewModel(store));
+            //modelSet.Add("QuestionViewModel", new QuestionViewModel(store));
+        }
+        /*
+        private async void InitializeStore(QuestionStore store)
+        {
+            await store.LoadQuestions();
+        }
+
+        /// <summary>
+        /// QuestionList (main page) view model. The QuestionListView is databound to this property.
+        /// </summary>
+        public QuestionListViewModel QuestionListViewModel
+        {
+            get
+            {
+                return (QuestionListViewModel)modelSet["QuestionListViewModel"];
+            }
+        }
+
+        /// <summary>
+        /// Question (detail page) view model. QuestionDetails page is databound to this property.
+        /// </summary>
+        public QuestionViewModel QuestionViewModel
+        {
+            get
+            {
+                return (QuestionViewModel)modelSet["QuestionViewModel"];
+            }
+        }
+        */
+    }
+}
